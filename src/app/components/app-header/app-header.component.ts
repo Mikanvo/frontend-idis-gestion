c1f3b7ae71eafab8a8824e0311a9022e39853c7c
KOMAN STEPHANE <stefchris2@gmail.com>
2018-04-05 12:18:12 +0100

    Update of app-header.component and adding of default.png

--- src/app/components/app-header/app-header.component.ts
+++ src/app/components/app-header/app-header.component.ts
@@ -3,7 +3,8 @@ import {AuthenticationService} from '../../services/auth/authentication.service'
 
 @Component({
   selector: 'app-header',
-  templateUrl: './app-header.component.html'
+  templateUrl: './app-header.component.html',
+  styleUrls: ['./app-header.component.scss']
 })
 export class AppHeaderComponent {
   constructor(
